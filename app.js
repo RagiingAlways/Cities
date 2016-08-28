ba1eae53eb55283d08f8ef5b1ffb1c46dac54097
HoeenCoder <orassuggestions@gmail.com>
2016-08-28 09:41:13 -0400

    Add WIP code

--- app.js
+++ app.js
@@ -1,16 +1,236 @@
 ﻿'use strict';
-const fs = require('fs');
+
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-global.io = require('socket.io')(http);
+var io = require('socket.io')(http);
+
+var c = require('./config.json');
+
+var sockets = {};
+var users = [];
+var resources = [];
+var workers = [];
+var soldiers = [];
+
+var leaderboard = [];
+var leaderboardUpdated = false;
 
 io.on('connection', function(socket) {
-
+	console.log('A user connected.');
+	
+	var radius = c.baseRadius;
+	var position = c.newPlayerInitialPosition == 'random' ? {x: Math.round(Math.random() * c.gameWidth), y: Math.round(Math.random() * c.gameHeight)} : c.newPlayerInitialPosition;
+	var hp = c.defaulthp, maxhp = c.defaulthp;
+	
+	var tents = [{
+		radius: c.baseRadius,
+		x: position.x,
+		y: position.y,
+		hp: hp,
+		maxhp: maxhp,
+	}];
+	var points = 0;
+	var level = c.defaultLvl > 0 ? c.defaultLvl : 1;
+	
+	var currentPlayer = {
+		id: socket.id,
+		x: position.x,
+		y: position.y,
+		tents: tents,
+		points: points,
+		level: level,
+		maxhp: maxhp,
+		hp: hp,
+		hue: Math.rounf(Math.random() * 360),
+		target: {x: 0, y: 0}
+	};
+	
+	socket.on('gotit', function(player) {
+		console.log('[INFO] ' + player.name + ' connecting.');
+		
+		if(users.findIndex(player.id) > -1) {
+			console.log('[INFO] Player ID is already connected, kicking.');
+            		socket.disconnect();
+		} else if(!validNick(player.name)) {
+			socket.emit('kick', 'Invalid username.');
+			socket.disconnect();
+		} else {
+			sockets[player.id] = socket;
+			
+			var radius = c.baseRadius;
+			var position = c.newPlayerInitialPosition == 'random' ? {x: Math.round(Math.random() * c.gameWidth), y: Math.round(Math.random() * c.gameHeight)} : c.newPlayerInitialPosition;
+			var hp = c.defaulthp, maxhp = c.defaulthp;
+			
+			player.x = position.x;
+			player.y = position.y;
+			player.target.x = 0;
+			player.target.y = 0;
+			var tents = [{
+				radius: c.baseRadius,
+				x: position.x,
+				y: position.y,
+				hp: hp,
+				maxhp: maxhp,
+			}];
+			
+			player.points = 0;
+			player.level = c.defaultLvl > 0 ? c.defaultLvl : 1;
+			player.hue = Math.round(Math.round() * 360);
+			currentPlayer = player;
+			users.push(currentPlayer);
+			
+			socket.emit('gameSetup', {
+				gameWidth: c.gameWidth,
+				gameHeight: c.gameHeight
+			});
+			console.log('Total players: ' + user.length);
+		}
+	});
+	
+	socket.on('windowResized', function (data) {
+		currentPlayer.screenWidth = data.screenWidth;
+		currentPlayer.screenHeight = data.screenHeight;
+	});
+	
+	socket.on('respawn', function() {
+		if(users.findIndex(currentPlayer.id) > -1) {
+			users.splice(users.findIndex(currentPlayer.id), 1);
+		}
+		socket.emit('welcome', currentPlayer);
+		console.log('[INFO] Users ' + currentPlayer.name + ' respawned.');
+	});
+	
+	socket.on('disconnect', function() {
+		if(users.findIndex(currentPlayer.id) > -1) {
+			users.splice(users.findIndex(currentPlayer.id), 1);
+		}
+		console.log('[INFO] User ' + currentPlayer.name + ' disconnected.');
+	});
 });
 
-app.use(express.static(__dirname + '/client/'));
+function tickPlayer(currentPlayer) {
+	movePlayer(currentPlayer);
+	
+	//more to add soon!
+}
 
-http.listen(3000, function() {
-	console.log('Now listening on port 3000');
-});
+function moveloop() {
+	for(var i = 0; i < users.length; i++) {
+		tickPlayer(users[i]);
+	}
+}
+
+function gameloop() {
+	if(users.length > 0) {
+		users.sort(function(a, b) { return b.points - a.points; });
+		
+		var topUsers = [];
+		
+		for(var i = 0; i < Math.min(10, users.length); i++) {
+			topUsers.push({
+				id: users[i].id,
+				name: users[i].name
+			});
+		}
+		if(isNaN(leaderboard) || leaderboard.length !== topUsers.length) {
+			leaderboard = topUsers;
+			leaderboardUpdated = true;
+		} else {
+			for(i = 0; i < leaderboard.length; i++) {
+				if(leaderboard[i].id !== topUsers[i].id) {
+					leaderboard = topUsers;
+					leaderboardUpdated = true;
+					break;
+				}
+			}
+		}
+	}
+}
+
+function validNick(nickname) {
+    var regex = /^\w*$/;
+    return regex.exec(nickname) !== null;
+};
+
+function sendUpdates() {
+	users.forEach(function(u) {
+		//center view if x/y is undefined (for player who died)
+		u.x = u.x || c.gameWidth / 2;
+		u.y = u.y || c.gameWidth / 2;
+		
+		//find visible things
+		var visibleResources = resources.map(function(f) {
+			if(f.x > u.x - u.screenWidth/2 - f.radius &&
+				f.x < u.x + u.screenWidth/2 + f.radius &&
+                    		f.y > u.y - u.screenHeight/2 - f.radius &&
+                    		f.y < u.y + u.screenHeight/2 + f.radius) {
+                    		return f;
+			}
+		}).filter(function(f) { return f; });
+		
+		var visibleWorkers = workers.map(function(f) {
+			if(f.x > u.x - u.screenWidth/2 - f.radius - 20 &&
+				f.x < u.x + u.screenWidth/2 + f.radius + 20 &&
+                    		f.y > u.y - u.screenHeight/2 - f.radius - 20 &&
+                    		f.y < u.y + u.screenHeight/2 + f.radius + 20) {
+                    		return f;
+			}
+		}).filter(function(f) { return f; });
+		
+		var visibleSoldiers = soldiers.map(function(f) {
+			if(f.x > u.x - u.screenWidth/2 - f.radius - 20 &&
+				f.x < u.x + u.screenWidth/2 + f.radius + 20 &&
+                    		f.y > u.y - u.screenHeight/2 - f.radius - 20 &&
+                    		f.y < u.y + u.screenHeight/2 + f.radius + 20) {
+                    		return f;
+			}
+		}).filter(function(f) { return f; });
+		
+		var visibleTents = users.map(function(f) {
+			for(var z = 0; z < f.tents.length; z++) {
+				if(f.tents[z].x + f.tents[z].radius > u.x - u.screenWidth/2 - 20 &&
+					f.tents[z].x - f.tents[z].radius < u.x + u.screenWidth/2 + 20 &&
+					f.tents[z].y + f.tents[z].radius > u.y - u.screenHeight/2 - 20 &&
+					f.tents[z].y - f.tents[z].radius < u.y + u.screenHeight/2 + 20) {
+						z = f.tents.length;
+						if(f.id !== u.id) {
+							return {id: f.id, x: f.x, y: f.y, tents: f.tents, hue: f.hue, name: f.name};
+						} else {
+							return {x: f.x, y: f.y, tents: f.tents, hue: f.hue};
+						}
+					}
+			}
+		}).filter(function(f) { return f; });
+		
+		sockets[u.id].emit('serverSayMove', visibleResources, visibleWorkers, visibleSoldiers, visibleTents);
+		if(leaderboardUpdated) {
+			sockets[u.id].emit('leaderboard', {
+				players: users.length,
+				leaderboard: leaderboard
+			});
+		}
+	});
+	leaderboardUpdated = false;
+}
+
+setInterval(moveloop, 1000 / 60);
+setInterval(gameloop, 1000);
+setInterval(sendUpdates, 1000 / c.networkUpdateFactor);
+
+app.use(express.static(__dirname + '/client'));
+
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || process.env.IP || '127.0.0.1';
+var serverport = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || c.port;
+if (process.env.OPENSHIFT_NODEJS_IP !== undefined) {
+    http.listen( serverport, ipaddress, function() {
+        console.log('Now listening on port ' + serverport);
+    });
+} else {
+    http.listen( serverport, function() {
+        console.log('Now listening on port ' + c.port);
+    });
+}
+/*http.listen(c.port, function() {
+	console.log('Now listening on port ' + c.port);
+});*/
--- client/script.js
+++ client/script.js
@@ -1,23 +1,150 @@
-$(function(){
+//var io = require('socket.io-client');
+var global = {};
+var socket, reason;
+var player = {
+	id: -1,
+	x: window.innerWidth / 2,
+	y: window.innerHeight / 2,
+	screenWidth: window.innerWidth,
+	screenHeight: window.innerHeight,
+	target: {x: window.innerWidth / 2, y: window.innerHeight / 2}
+};
+global.player = player;
 
-    $('.nickname').keypress(function(e){
-      if(e.which === 13){
-        $('.ready').click();
-      }
-    });
+var resources = [];
+var workers = [];
+var soldiers = [];
+var users = [];
+var leaderboard = [];
+var target = {x: player.x, y: player.y};
 
-    $('.ready').click(function(){
-        var nick = $('.nickname').val();
-        //blank nickname
-        if(nick.trim() === ''){
-            return;
-        }
-
-        var c = document.createElement('canvas');
-        var ctx = c.getContext('2d');
-
-        $(c).attr('id', 'map').attr('width', '1400').attr('height', '1000');
-        $('.main').html(c);
-	$('.main').css('margin', 0);
-    });
+$('.nickname').keypress(function(e){
+  if(e.which === 13){
+    $('.ready').click();
+  }
 });
+
+$('.ready').click(function(){
+  var nick = $('.nickname').val();
+  //blank nickname
+  if(nick.trim() === ''){
+    nick = 'An unnamed city.';
+  }
+
+  global.c = document.createElement('canvas');
+  window.graph = global.c.getContext('2d');
+  $(global.c).attr('id', 'map').attr('width', '1400').attr('height', '1000');
+  $('.main').html(global.c);
+  $('.main').css('margin', 0);
+  //TEMP CODE
+  drawgrid();
+  //END TEMP CODE
+  startGame(nick);
+  
+  /*graph.fillStyle = '#00EE00';
+  graph.fillRect(0, 0, window.innerWidth, window.innerHeight);
+  drawgrid();
+  drawCircle((window.innerWidth / 2), (window.innerHeight / 2), 40, 5, 240);*/
+});
+
+function startGame(name) {
+	global.playerName = name.replace(/(<([^>]+)>)/ig, '').substring(0,25);
+	
+	global.screenWidth = window.innerWidth;
+	global.screenHeight = window.innerHeight;
+	
+	if(!socket) {
+		socket = io();
+		setUpSocket(socket);
+	}
+	//if(!global.animLoopHandle) animLoop();
+	//socket.emit('respawn');
+	global.socket = socket;
+}
+
+function setUpSocket(socket) {
+	socket.on('connect-failed', function() {
+		socket.close();
+		global.disconnected = true;
+	});
+	
+	socket.on('disconnect', function() {
+		socket.close();
+		global.disconnected = true;
+	});
+	
+	socket.on('welcome', function(playerSettings) {
+		player = playerSettings;
+		player.name = global.playerName;
+		player.screenWidth = global.screenWidth;
+		player.screenHeight = global.screenHeight;
+		//player.target = window.canvas.target;
+		global.player = player;
+		socket.emit('gotit', player);
+		global.gameStart = true;
+		global.c.focus();
+	});
+	
+	socket.on('gameSetup', function(data) {
+		global.gameWidth = data.gameWidth;
+		global.gameHeight = data.gameHeight;
+		resize();
+	});
+	
+	socket.on('serverSayMove', function(resourceData, workerData, soldierData, tentData) {
+		var playerData;
+		for(var i = 0; i < tentData.length; i++) {
+			
+		}
+	});
+}
+
+function drawCircle(centerX, centerY, radius, sides, hue) {
+    var theta = 0;
+    var x = 0;
+    var y = 0;
+    let graph = window.graph;
+    graph.lineWidth = 10;
+    graph.strokeStyle = 'hsl(' + hue + ', 100%, 40%)';
+    graph.fillStyle = 'hsl(' + hue + ', 100%, 50%)';
+    
+    graph.beginPath();
+
+    for (var i = 0; i < sides; i++) {
+        theta = (i / sides) * 2 * Math.PI;
+        x = centerX + radius * Math.sin(theta);
+        y = centerY + radius * Math.cos(theta);
+        graph.lineTo(x, y);
+    }
+
+    graph.closePath();
+    graph.stroke();
+    graph.fill();
+}
+
+function drawgrid() {
+    let graph = window.graph;
+    graph.lineWidth = 1;
+    graph.strokeStyle = 130;
+    graph.globalAlpha = 0.15;
+    graph.beginPath();
+
+    for (var x = -0 - player.x; x < window.innerWidth; x += window.innerHeight / 18) {
+        graph.moveTo(x, 0);
+        graph.lineTo(x, window.innerHeight);
+    }
+
+    for (var y = -0 - player.y ; y < window.innerHeight; y += window.innerHeight / 18) {
+        graph.moveTo(0, y);
+        graph.lineTo(window.innerWidth, y);
+    }
+
+    graph.stroke();
+    graph.globalAlpha = 1;
+}
+
+function resize() {
+	player.screenWidth = c.width = global.screenWidth = global.playerType == 'player' ? window.innerWidth : global.gameWidth;
+	player.screenHeight = c.height = global.screenHeight = global.playerType == 'player' ? window.innerHeight : global.gameHeight;
+	socket.emit('windowResized', { screenWidth: global.screenWidth, screenHeight: global.screenHeight });
+}
